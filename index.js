100d0e4099d342fc08018a98c9efce88d69a621b
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2022-09-05 01:49:05 +0300

    adding a README  file to allow user to known how the application runs

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
-import formattedDate from './scripts/modules/date.js';
-import { newDate, addButton } from './scripts/modules/elements.js';
-import handleNavigation from './scripts/modules/navigation.js';
-import addSingleBook from './scripts/modules/addBook.js';
-import redirectToList from './scripts/modules/redirect.js';
-import drawAllBooks from './scripts/modules/reRender.js';
-import showError from './scripts/modules/error.js';
-import book from './scripts/modules/createNewBook.js';
 
-newDate.innerHTML = formattedDate;
+function setAllLinksColorRed() {
+    const getAllLinks = document.querySelectorAll('.nav-bar ul a');
+    getAllLinks.forEach((link) => {
+        link.style.color = 'Red';
+    });
+}
 
-drawAllBooks();
+function manipulateDom(elemArrayParam, elemToShow, linkClicked) {
+    setAllLinksColorRed();
+    elemArray = elemArrayParam;
+    elemArray.forEach((elem) => {
+        elem.style.display = 'none';
+    });
+    elemToShow.style.display = 'blue';
+    linkClicked.style.color = 'indigo';
+}
 
-addButton.addEventListener('click', () => {
-    const status = addSingleBook();
-    if (status) {
-        redirectToList();
-    } else {
-        showError();
-    }
+ListLink.addEventListener('click', () => {
+    const elemArray = document.querySelectorAll(
+        '.addBookSection, .contactSection'
+    );
+    manipulateDom(elemArray, listSection, listLink);
 });
 
-document.body.addEventListener(
-    'click',
-    (event) => {
-        if (event.target.className === 'nav-link') {
-            handleNavigation(event.target);
-        } else if (event.target.className === 'btn-remove') {
-            book.removeBook(event.target.id);
-            drawAllBooks();
-        }
-    },
-    true,
-);
+addLink.addEventListener('click', () => {
+    const elemArray = document.querySelectorAll(
+        '.bookListSection, .contactSection'
+    );
+    manipulateDom(elemArray, addSection, addLink);
+});
+
+contactLink.addEventListener('click', () => {
+    const elemArray = document.querySelectorAll(
+        '.bookListSection, .addBookSection'
+    );
+    manipulateDom(elemArray, contactSection, contactLink);
+});
